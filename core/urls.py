68749466dfb49bdba66bd70afc98bd821e78b5b2
M Haziq <m_haziq@outlook.com>
2023-10-23 15:45:17 +0500

    setup video management project.

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/v1/account/', include('api.v1.accounts.urls')),
+    path('api/v1/video/', include('api.v1.videos.urls')),
+]
