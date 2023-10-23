2ab221891615a23ae992fc8fbaa10048013e9fd4
M Haziq <m_haziq@outlook.com>
2023-10-23 20:51:43 +0500

    cover the expose port to admin panel.

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 
     path('api/v1/account/', include('api.v1.accounts.urls')),
     path('api/v1/video/', include('api.v1.videos.urls')),
