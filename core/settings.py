ddf814827dbaede5f141afa07a705c72f1a4e8b0
M Haziq <m_haziq@outlook.com>
2023-10-24 01:35:57 +0500

    update setting to get rid off user and password.

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
     }
